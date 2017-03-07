c0af5278856eed053f98b6e2046631d10bd917f1
djtekno11 <djtekno11@gmail.com>
2017-03-07 11:22:22 -0600

    Set up initial template from earlier commit, added initinal info, created and added cipher.txt and key.txt (look in resource filter).

--- prog4Secretvfong3/prog4Secretvfong3/prog4Secretvfong3.cpp
+++ prog4Secretvfong3/prog4Secretvfong3/prog4Secretvfong3.cpp
@@ -1,11 +1,39 @@
-// prog4Secretvfong3.cpp : Defines the entry point for the console application.
-//
+//	Author: Victor Fong & Aswin Manoj
+//	Assignment : #3, Pentago
+//	TA : Nianzu Ma, Tues 4PM
+//	Mar 7, 2017
 
-#include "stdafx.h"
+
+//include libraries
+#include <cstdio>
+#include <cstdlib>
+#define ROWS 3
+#define COLUMNS 3
+
+
+void displayInformationAndInstructions(void)   //function will display programmer information of the program.
+{
+	printf("Welcome to the decoding program, where hidden messages are found inside of a text file. \n");
+	printf("\n");
+	
+	printf("Author: Victor Fong & Aswin Manoj \n");
+	printf("Assignment : #4, Secret \n");
+	printf("TA : Nianzu Ma, Tues 4PM \n");
+	printf("Feb 19, 2017 \n");
+	printf("\n");
+
+	return;
+}
 
 
 int main()
 {
-    return 0;
+	displayInformationAndInstructions();
+    
+	
+	
+	
+	
+	
+	return 0;
 }
-
