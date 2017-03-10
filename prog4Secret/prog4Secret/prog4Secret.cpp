1cd2a34c624c297a52d901d235f5274676ca202f
vfong3 <vfong3@uic.edu>
2017-03-10 15:42:46 -0600

    NEEDS WORK - significant changes

--- prog4Secret/prog4Secret/prog4Secret.cpp
+++ prog4Secret/prog4Secret/prog4Secret.cpp
@@ -12,6 +12,10 @@
 #include <iostream>
 using namespace std;
 
+#define MAX_WORDS_IN_KEY 20
+#define MAX_WORD_SIZE 25
+#define MAX_CHARACTERS_IN_CIPHER 17424
+
 void displayInformationAndInstructions(void)   //function will display programmer information of the program.
 {
 	cout << "Welcome to the decoding program, where hidden messages are found inside of a text file. \n";
@@ -28,38 +32,70 @@ void displayInformationAndInstructions(void)   //function will display programme
 
 
 
-//TODO: find out how to make character arrays correct number of elements for text file
 int analyzeKey(FILE* txtFile, char *keyArray) //takes in file and gets the number of strings, as well as what they are. returns number of characters in file (including added '\0')
 {
 	int i = 0;
 	int j = 0;
 	int wordCount = 1;
 	int keyCharacterCount = 0;
-	int keyIndex[2];
-
+	int length = 0;
+	
+	char *pSpace = NULL;
+    char *pCurrent;
 	char tempChar = 0;
+	char keyWords[MAX_WORDS_IN_KEY][MAX_WORD_SIZE] = { NULL };
+	
+
+
+
 
 	while (fscanf(txtFile, "%c", &tempChar) != EOF)
 	{
 		keyArray[i] = tempChar;
 
 		if (keyArray[i] == ' ')
-		{
-			//j++;
-			wordCount++;
-		}
+		{ wordCount++; }
 		i++;
+		keyCharacterCount++;
 	}
 	keyArray[i] = '\0';
-	keyCharacterCount++;
 
-	cout << "Read in " << wordCount << " keyWords, which are: " << keyArray;
+//Find the address of the first space
+    pSpace = strchr( keyArray, ' ');
+//Find out how many characters are between the space and the beginning of the array
+    length = pSpace - keyArray;
+//Copy that number of charactes into word1
+    strncpy( keyWords[0], keyArray, length);
+//Display the first word that has been found
+	cout << "Read in " << wordCount << " keyWords, which are:\n"
+		 << keyWords[0];
+     
+	for (int i = 1; i < wordCount; i++)
+	{
+		pCurrent = pSpace + 1;
+		pSpace = strchr(pCurrent, ' ');
+		length = pSpace - pCurrent;
+		strncpy( keyWords[i], pCurrent, length);
+	//Display the second word that has been found
+		printf("%s\n", keyWords[i]);
+	}
 
+	
+	
+	
+/*	sscanf(keyArray, "%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s", word1, word2, word3, word4, word5, word6, word7, word8, word9, word10, word11, word12, word13, word14, word15, word16, word17, word18, word19, word20);
+	cout << "Read in " << wordCount << " keyWords, which are:\n" << word1  << "\n" << word2  << "\n" << word3  << "\n" << word4  << "\n" << word5  << "\n"
+		 << word6  << "\n" << word7  << "\n" << word8  << "\n" << word9  << "\n" << word10  << "\n"
+		 << word11  << "\n" << word12  << "\n" << word13  << "\n" << word14  << "\n" << word15 << "\n"
+		 << word16  << "\n" << word17  << "\n" << word18  << "\n" << word19 << word20;
+
+	cout << &word1 << "\n" << &word2;
+
+	*/
 	return keyCharacterCount;
 }
 
 
-//TODO: find out how to make character arrays correct number of elements for text file
 int analyzeCipher(FILE* txtFile, char *cipherArray) //takes in file and gets the number of strings, as well as what they are. Returns number of characters in taken string
 {
 	int i = 0;
@@ -85,7 +121,7 @@ char promptForChoiceAndScanForSelection() {
 
 	cout << "Choose from the following options:\n"
 		 << "	1. Display a particular sized table\n"
-		 << "	2. Find all matching key word phrases\n"
+		 << "	2. Find all matching key word keyArrays\n"
 		 << "	X. Exit the program\n"
 		 <<	"Enter your choice -> "; cin >> selection;
 
@@ -134,8 +170,8 @@ int main()
 	int cipherCharacterCount = 0;
 	int numColumns = 0;
 
-	char keyArray[150];
-	char cipherArray[500];
+	char keyArray[MAX_WORDS_IN_KEY * MAX_WORD_SIZE] = { NULL };
+	char cipherArray[MAX_CHARACTERS_IN_CIPHER] = { NULL };
 	char tempChar = ' ';
 	char input = ' ';
 
